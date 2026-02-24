3159a037419bbcc31cf917dc5372013107dd032c
revaninurulfadilla-afk <revaninurulfadilla@gmail.com>
2026-02-24 16:08:22 +0700

    pertemuan 2

---
+++ application/controllers/kategori.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH')OR exit('No direct script access allowed');
+
+class kategori extends CI_Controller{
+
+    public function __construct()
+    {
+        parent ::__construct();
+        $this->load->model('Kategori_model');
+
+    }
+    public function index()
+    {
+        $data['kategori'] = $this->Kategori_model->get_all();
+        $this->load->view('kategori/index', $data);
+
+    }
+}
