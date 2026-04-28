b35695dbd7ed71bb999183d60708a2a37d8172e2
revaninurulfadilla-afk <revaninurulfadilla@gmail.com>
2026-04-28 16:09:19 +0700

    pertemuan 9

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,16 +1,24 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class dashboard extends CI_Controller{
+class Dashboard extends CI_Controller {
 
-public function __construct()
+    public function __construct()
 {
-    parent::__construct();
+    parent ::__construct();
+    if (!$this->session->userdata('login')){
+        redirect('login');
+    }
 }
+
 public function index()
 {
-    $data['total_kategori']= $this->db->count_all('kategori');
-    $data['total_buku']= $this->db->count_all('buku');
+    $data['total_kategori'] = $this->db->count_all('kategori');
+    $data['total_buku']  = $this->db->count_all('buku'); 
+
+    // ambil data
+    $data['kategori'] = $this->db->get('kategori')->result();
+    $data['buku']  = $this->db->get('buku')->result();
 
     $this->load->view('templates/header');
     $this->load->view('templates/sidebar');
